15bbc606b707cf965f3f30156fe45e31ca49819b
Zeineb2727 <zeinebcosgun9@gmail.com>
2025-09-02 16:16:22 +0200

    Organisez le code PHP en blocs

--- index.php
+++ index.php
@@ -8,22 +8,7 @@
     <body>
  
     <!-- L'en-tête -->
-    
-    <header>
-        <!-- Le menu -->
-    
-        <nav id="menu">        
-            <div class="element_menu">
-                <h3>Titre menu</h3>
-                <ul>
-                    <li><a href="page1.html">Lien</a></li>
-                    <li><a href="page2.html">Lien</a></li>
-                    <li><a href="page3.html">Lien</a></li>
-                </ul>
-            </div>    
-        </nav>
-       
-    </header>
+    <?php include('header.php'); ?>
     
     <!-- Le corps -->
     
@@ -37,9 +22,7 @@
     
     <!-- Le pied de page -->
     
-    <footer id="pied_de_page">
-        <p>Copyright moi, tous droits réservés</p>
-    </footer>
+    <?php include('footer.php'); ?>
     
     </body>
 </html>
